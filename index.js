ea6af7d152b9bf71595778845bc24c0d6383ca06
Me6111 <maksymmpawlowski@gmail.com>
2024-06-20 23:08:45 +0100

    Updated app.js

--- index.js
+++ index.js
@@ -5,8 +5,11 @@
 
 
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const port = 3000;
+
+app.use(cors()); 
 
 app.get('/hello', (req, res) => {
   res.send('Hello client');
