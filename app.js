3fcfbe4de7e778cfb582b1c9c28bebba1b9c6bcc
Me6111 <maksymmpawlowski@gmail.com>
2024-06-21 01:28:00 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-
-
-
-// C:\Users\user\Desktop\projects\wftd-express\index.js
-
+// C:\Users\user\Desktop\projects\wftd-express\app.js
 
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Dynamically set allowed origins based on environment
-const allowedOrigins = ['http://localhost:3000']; // Add other origins as needed
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+// Simplified CORS configuration to accept requests from any origin
+app.use(cors());
 
 app.get('/hello', (req, res) => {
   res.send('Hello client');
