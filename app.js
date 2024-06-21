a6239c7f9bf3da5900e70cfdbdd89a82094c61d1
Me6111 <maksymmpawlowski@gmail.com>
2024-06-21 01:10:59 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -5,11 +5,25 @@
 
 
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors()); 
+// Dynamically set allowed origins based on environment
+const allowedOrigins = ['http://localhost:3000']; // Add other origins as needed
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.get('/hello', (req, res) => {
   res.send('Hello client');
